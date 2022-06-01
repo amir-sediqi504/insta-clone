2490f96fee1ee90631adab7e5b855a5502b2afdb
Amir Sediqi <amir.sediqi69@gmail.com>
2022-06-01 03:23:07 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,84 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+
+import { View, Text } from 'react-native'
+
+import * as firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBDbL2PjiVQljp1aDxHGBh1t9-DYDwdPuk",
+  authDomain: "instagrammie-65597.firebaseapp.com",
+  projectId: "instagrammie-65597",
+  storageBucket: "instagrammie-65597.appspot.com",
+  messagingSenderId: "24804335567",
+  appId: "1:24804335567:web:6c2e5887805e2e0906bcba",
+  measurementId: "G-GDD3RFGV1C"
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
+
+
+const Stack = createStackNavigator();
+
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+    }
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        })
+      }
+    })
+  }
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if (!loaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Text>Loading</Text>
+        </View>
+      )
+    }
+
+    if (!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
+
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Text>User is logged in</Text>
+      </View>
+    )
+  }
+}
+
+export default App
